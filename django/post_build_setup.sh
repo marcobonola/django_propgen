500a49b81f62ca6078005ebd311811514b9c9b75
marcobonola <marco.bonola@gmail.com>
2018-03-19 00:15:28 +0100

    django dummy superuser created at first boot (admin, admin) + minor fix

--- django/post_build_setup.sh
+++ django/post_build_setup.sh
@@ -1,17 +1,23 @@
 if [ -f .post_build_setup_done ]; 
   then
-    echo "migration and fixutres already installed"
+    echo "post build setup already done"
   else
     echo "sleeping 20 seconds"
     sleep 20
+
     echo "collect static files"
     python manage.py collectstatic --no-input
+
     echo "prepare and apply django migrations"
     python manage.py makemigrations && python manage.py migrate
+
     echo "installing fixtures"
     python manage.py loaddata proposal/fixtures/project.json
     python manage.py loaddata proposal/fixtures/settings.json
     python manage.py loaddata proposal/fixtures/template.json
     python manage.py loaddata proposal/fixtures/textblock.json 
     touch .post_build_setup_done
+
+    echo "create super user"
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', '', 'admin')" | ./manage.py shell
 fi
