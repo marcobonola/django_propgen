b701dbafd4d9486c5b2fcf8a4694dad720699219
Christian Stroehmeier <stroemi@uni-paderborn.de>
2018-03-20 02:52:50 +0100

    Fixed PDF generation
    - main.tex was using settings that did not exist which caused pdflatex to fail
    - there was another image referenced, inserted dummy
    - added download-button for bundle and single PDFs
    - nginx has to deliver produced files, so mounted /media on the backend as well. This seems to be a good idea in general, uploaded files should stay in the container anyways

--- ng-propgen/src/propgen/components/menu/dialogs/run-latex.dialog.component.ts
+++ ng-propgen/src/propgen/components/menu/dialogs/run-latex.dialog.component.ts
@@ -1,5 +1,6 @@
 import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {DefaultBackendService} from '../../../services/default-backend.service';
 
 export type ProcessResult = {
   stdout: string;
@@ -34,20 +35,29 @@ export type RunLatexResponse = {
   '  <mat-accordion>' +
   '    <mat-expansion-panel *ngFor="let d of data">' +
   '      <mat-expansion-panel-header>' +
-  '        <mat-panel-title>{{d.obj.name}}</mat-panel-title>' +
-  '        <mat-panel-description>{{d.pdf}} - {{d.result}}</mat-panel-description>' +
+  '        <mat-panel-title>' +
+  '          <a *ngIf="d.result === \'ok\'" href="{{ backend }}/media/produced/{{ d.pdf }}" target="_blank">' +
+  '            <mat-icon>get_app</mat-icon>' +
+  '          </a>' +
+  '          {{d.obj.name}}' +
+  '        </mat-panel-title>' +
+  '        <mat-panel-description>' +
+  '          {{d.pdf}} - {{d.result}}' +
+  '        </mat-panel-description>' +
   '      </mat-expansion-panel-header>' +
-  '      <h4>pdftex (Return code {{d.run1.returncode}})</h4>' +
+  '      <h4>pdftex</h4>' +
   '      <pre>{{d.run1.result.stdout}}</pre>' +
-  '      <h4>bibtex (Return code {{d.bibtex.returncode}})</h4>' +
+  '      <h4>bibtex</h4>' +
   '      <pre>{{d.bibtex.result.stdout}}</pre>' +
   '    </mat-expansion-panel>' +
   '  </mat-accordion>' +
   '</div>' +
   '<div mat-dialog-actions>' +
   '  <button mat-raised-button mat-dialog-close>Ok</button>' +
+  '  <a mat-raised-button href="{{ backend }}/media/produced/output.tgz">Download all files</a>' +
   '</div>',
 })
 export class RunLatexDialogComponent {
+  public backend = DefaultBackendService.baseUrl;
   constructor(private dialogRef: MatDialogRef<RunLatexDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: RunLatexResponse[]) {}
 }
